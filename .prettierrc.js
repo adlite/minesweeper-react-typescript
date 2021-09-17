ba793f8d5c12d1b0a359021e45ecc1bfa18dc9f0
Adlite <igor@chillicode.ru>
2021-09-18 01:23:42 +0300

    Init prettier, eslint and lint-staged

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/**
+ * Prettier config file
+ * @see https://prettier.io/docs/en/index.html
+ * @see https://prettier.io/docs/en/options.html
+ */
+module.exports = {
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'all',
+  jsxBracketSameLine: true,
+  bracketSpacing: false,
+  tabWidth: 2,
+  semi: true,
+};
