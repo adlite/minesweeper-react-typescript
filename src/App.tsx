1a9bf46f5bee204fd51f1f4f1fd06cd591014eaf
Adlite <igor@chillicode.ru>
2021-09-18 01:25:20 +0300

    Test lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Test lint-staged</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
